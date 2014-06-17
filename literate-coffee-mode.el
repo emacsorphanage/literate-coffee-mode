94b709d627c5a0d600c15121006b093ecaa690fc
Syohei YOSHIDA <syohex@gmail.com>
2014-06-17 19:44:06 +0900

    Fix syntax-table property issue(#2)

--- literate-coffee-mode.el
+++ literate-coffee-mode.el
@@ -26,16 +26,78 @@
 
 (require 'coffee-mode)
 
+(defun litcoffee--font-lock-regexp (limit)
+  (and (re-search-forward coffee-regexp-regexp limit t)
+       (goto-char (match-end 0))
+       (not (get-text-property (point) 'litcoffee-not-highlight))))
+
+(defun litcoffee--font-lock-this (limit)
+  (and (re-search-forward coffee-this-regexp limit t)
+       (goto-char (match-end 0))
+       (not (get-text-property (point) 'litcoffee-not-highlight))))
+
+(defun litcoffee--font-lock-prototype (limit)
+  (and (re-search-forward coffee-prototype-regexp limit t)
+       (goto-char (match-end 0))
+       (not (get-text-property (point) 'litcoffee-not-highlight))))
+
+(defun litcoffee--font-lock-assign (limit)
+  (and (re-search-forward coffee-assign-regexp limit t)
+       (goto-char (match-end 0))
+       (not (get-text-property (point) 'litcoffee-not-highlight))))
+
+(defun litcoffee--font-lock-local-assign (limit)
+  (and (re-search-forward coffee-local-assign-regexp limit t)
+       (goto-char (match-end 1))
+       (not (get-text-property (point) 'litcoffee-not-highlight))))
+
+(defun litcoffee--font-lock-boolean (limit)
+  (and (re-search-forward coffee-boolean-regexp limit t)
+       (goto-char (match-end 0))
+       (not (get-text-property (point) 'litcoffee-not-highlight))))
+
+(defun litcoffee--font-lock-lambda (limit)
+  (and (re-search-forward coffee-lambda-regexp limit t)
+       (goto-char (match-end 1))
+       (not (get-text-property (point) 'litcoffee-not-highlight))))
+
+(defun litcoffee--font-lock-keywords (limit)
+  (and (re-search-forward coffee-keywords-regexp limit t)
+       (goto-char (match-end 1))
+       (not (get-text-property (point) 'litcoffee-not-highlight))))
+
+(defun litcoffee--font-lock-string-interpolation (limit)
+  (and (re-search-forward coffee-string-interpolation-regexp limit t)
+       (goto-char (match-end 0))
+       (not (get-text-property (point) 'litcoffee-not-highlight))))
+
+(defvar litcoffee-font-lock-keywords
+  `((litcoffee--font-lock-regexp 0 font-lock-constant-face)
+    (litcoffee--font-lock-this 0 font-lock-variable-name-face)
+    (litcoffee--font-lock-prototype 0 font-lock-type-face)
+    (litcoffee--font-lock-assign 0 font-lock-type-face)
+    (litcoffee--font-lock-local-assign 1 font-lock-variable-name-face)
+    (litcoffee--font-lock-boolean 0 font-lock-constant-face)
+    (litcoffee--font-lock-lambda 1 font-lock-function-name-face)
+    (litcoffee--font-lock-keywords 1 font-lock-keyword-face)
+    (litcoffee--font-lock-string-interpolation 0 font-lock-variable-name-face t)))
+
 (defun litcoffee-syntax-propertize-function (start end)
   (goto-char start)
   (funcall
    (syntax-propertize-rules
     ("^\\(.\\{4\\}\\).*$"
      (0 (ignore
-         (let ((head-of-line (match-string 1)))
+         (let ((head-of-line (match-string 1))
+               (start (match-beginning 0))
+               (end (match-end 0)))
            (unless (string-match-p "^\\(\t\\| \\{4,\\}\\)" head-of-line)
-             (put-text-property (match-beginning 0) (match-end 0)
-                                'syntax-table (string-to-syntax "w")))))))
+             (put-text-property start end 'litcoffee-not-highlight t)
+             (save-excursion
+               (goto-char start)
+               (while (re-search-forward "['\"]" end t)
+                 (put-text-property (match-beginning 0) (match-end 0)
+                                    'syntax-table (string-to-syntax "_")))))))))
     (coffee-block-strings-delimiter
      (0 (ignore (coffee-syntax-block-strings-stringify))))
     ("\\(?:[^\\]\\)\\(/\\)"
@@ -59,6 +121,7 @@
 ;;;###autoload
 (define-derived-mode litcoffee-mode coffee-mode "LitCoffee"
   "Major mode for editing Literate CoffeeScript."
+  (setq font-lock-defaults '((litcoffee-font-lock-keywords)))
   (set (make-local-variable 'syntax-propertize-function)
        'litcoffee-syntax-propertize-function))
 
